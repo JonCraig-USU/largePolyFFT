ace3b3c645e728ab0a0f7db25b2f1d0bb3f42057
Jonathan <jonathanwcj413@outlook.com>
2021-11-08 23:19:52 -0700

    finished but there may be an error in python

--- README.md
+++ README.md
@@ -7,12 +7,13 @@
 - small problems checked and commented out
 
 3) Repeat the timing experiment that we did for the two previous methods (school book and 3subproblem) for the FFT method. Plot a graph showing all three methods, where each method's results line goes up to a maximum run time per problem (say around 20 minutes of CPU time). At this run time limit, what are the problem sizes that can be solved by the three methods? (15 points)
--  
+- The school book and the recursive did not get far past the mid teens while the FFT and the python were able to get into the early 20's
 
 4) Perform an accuracy study comparing the native Python multiply algorithm with the your implementation using FFT. Here for each multiply, use the function convertBack(digits, base) to convert N0 and N1 (a list of digits) into a standard Python long int (with base set to 2**11), then multiply the numbers and check that the solution is the same. See the code snippet below. You may find that at a certain size of number, the FFT starts to get different (and wrong) answers back. This is due to limited floating point mathematical operations. Does the FFT make errors? If so, at what size? (15 points)
-
+- I did not see any errors but did not run it past the mid 20's. I would think that there would be errors eventually but as the floating point is a 32 bit (I think) I would expect there to be no errors until into the 30's for powers of 2.
 
 5) Perform a timing study with increasing powers of 2 (see code snippet below). You will see that the built in Python code is faster than the FFT. What is the slope of the two methods? Based on this, what algorithm do you think Python is using? (15 points)
+FFT: .022 n ^ 1.041
+Python: 0.000117 n ^ 1.488
+So I may have done something wrong. Python's built in was WAY faster at the lower numbers but eventually slowed down at the higher numbers. Please inform me if you find the mistake.
 
-
-6) Document and submit your code (20 points)
--- timing.py
+++ timing.py
@@ -1,6 +1,6 @@
 from FFT import getComplex
 from polyMul import poly3, polySchool
-from polymulitply_FFT import polyMultiply, createNum
+from polymulitply_FFT import convertBack, polyMultiply, createNum
 import datetime
 import sys
 import matplotlib.pyplot as plt
@@ -22,42 +22,65 @@ def fftTime(n):
     b = createNum(n)
     polyMultiply(a, b, n, getComplex(n*2))
 
+def pythonTime(n):
+    a = createNum(n)
+    b = createNum(n)
+    an = convertBack(a)
+    bn = convertBack(b)
+    an * bn
+
 empTL = []
 empS = []
-for n in [2**5, 2**6, 2**7, 2**8]:
+sizes = [2**i for i in range(5, 13)]
+for n in sizes:
     startTime = datetime.datetime.now()
     schoolTime(n)
     endTime = datetime.datetime.now()
     time_diff = (endTime - startTime)
     elapsed = time_diff.total_seconds() * 1000
-    if elapsed > 0: #sometimes the function is too fast and we get 0 time
+    if elapsed > 0: 
         empTL.append(elapsed)
         empS.append(n)
 
 poly3TL = []
 poly3S = []
-for n in [2**6, 2**7, 2**8, 2**9]:
+sizes = [2**i for i in range(7, 13)]
+for n in sizes:
     startTime = datetime.datetime.now()
     poly3Time(n)
     endTime = datetime.datetime.now()
     time_diff = (endTime - startTime)
     elapsed = time_diff.total_seconds() * 1000
-    if elapsed > 0: #sometimes the function is too fast and we get 0 time
+    if elapsed > 0: 
         poly3TL.append(elapsed)
         poly3S.append(n)
 
 fftTL = []
 fftS = []
-sizes = [2**i for i in range(7, 26)]
+sizes = [2**i for i in range(7, 17)]
 for n in sizes:
     startTime = datetime.datetime.now()
     fftTime(n)
     endTime = datetime.datetime.now()
     time_diff = (endTime - startTime)
     elapsed = time_diff.total_seconds() * 1000
-    if elapsed > 0: #sometimes the function is too fast and we get 0 time
+    if elapsed > 0: 
         fftTL.append(elapsed)
         fftS.append(n)
+
+
+pythonTL = []
+pythonS = []
+sizes = [2**i for i in range(7, 18)]
+for n in sizes:
+    startTime = datetime.datetime.now()
+    pythonTime(n)
+    endTime = datetime.datetime.now()
+    time_diff = (endTime - startTime)
+    elapsed = time_diff.total_seconds() * 1000
+    if elapsed > 0: 
+        pythonTL.append(elapsed)
+        pythonS.append(n)
 
 
 plt.xlabel("n")
@@ -68,6 +91,7 @@ plt.xscale('log')
 plt.plot(empS, empTL, 'g')
 plt.plot(poly3S, poly3TL, 'b')
 plt.plot(fftS, fftTL, 'r')
+plt.plot(pythonS, pythonTL, 'b')
 
 
 plt.rcParams["figure.figsize"] = [16,9]
@@ -81,4 +105,7 @@ print("Poly 3 Sub Parts Time = %.6f n ^ %.3f" % (np.exp(intercept), slope))
 
 slope, intercept, _, _, _ = stats.linregress([np.log(v) for v in fftS], [np.log(t) for t in fftTL])
 print("FFT Time = %.6f n ^ %.3f" % (np.exp(intercept), slope))
+
+slope, intercept, _, _, _ = stats.linregress([np.log(v) for v in pythonS], [np.log(t) for t in pythonTL])
+print("Python Time = %.6f n ^ %.3f" % (np.exp(intercept), slope))
 
